a1411ff8645b55243691c4b0221e8eedb7f5ec47
Matthew Ellis <Matthew.Ellis@arm.com>
2026-01-30 16:52:49 +0000

    refactor(spi): add type to context params
    Declare SPI transport functions to accept a typed context rather
    than a void pointer.
    
    Change-Id: I421e007a7c57cedc8b847942d8c91035af009997
    Signed-off-by: Matthew Ellis <Matthew.Ellis@arm.com>

--- include/transport/spi.h
+++ include/transport/spi.h
@@ -7,22 +7,20 @@
 #ifndef TPM_SPI_TRANSPORT_H
 #define TPM_SPI_TRANSPORT_H
 
-struct tpm_spi_ops {
-	int (*get_access)(void *ctx);
-	void (*release_access)(void *ctx);
-	void (*start)(void *ctx);
-	void (*stop)(void *ctx);
-	int (*xfer)(void *ctx, unsigned int bytelen,
-		    const void *dout, void *din);
-};
-
 struct tpm_spi_priv;
+
+struct tpm_spi_ops {
+	int (*get_access)(struct tpm_spi_priv *ctx);
+	void (*release_access)(struct tpm_spi_priv *ctx);
+	void (*start)(struct tpm_spi_priv *ctx);
+	void (*stop)(struct tpm_spi_priv *ctx);
+	int (*xfer)(struct tpm_spi_priv *ctx, unsigned int bytelen,
+		    const unsigned char *dout, unsigned char *din);
+};
 
 struct tpm_spi_plat {
 	struct tpm_spi_priv *priv;
 	const struct tpm_spi_ops *ops;
 };
-
-
 
 #endif /* TPM_SPI_TRANSPORT_H */
