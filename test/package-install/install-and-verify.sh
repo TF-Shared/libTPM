9e4c53a65766427574b13740df0bcf08f3f0b089
Matthew Ellis <Matthew.Ellis@arm.com>
2026-02-10 12:06:40 +0000

    build: correct location for tpm_platform.h
    Update CMake install to place tpm_platform.h correctly.
    Update install test script to avoid using cached CMake parameters.
    
    Signed-off-by: Matthew Ellis <Matthew.Ellis@arm.com>
    Change-Id: I21e74e3724b2052bd311def1e3159241f5ad0165

--- test/package-install/install-and-verify.sh
+++ test/package-install/install-and-verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2025, Arm Limited. All rights reserved.
+# Copyright (c) 2025-2026, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -9,12 +9,13 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TEST_INSTALL_DIR="${SCRIPT_DIR}"
-TEST_INSTALL_BUILD_DIR="${TEST_INSTALL_DIR}/build"
+TEST_INSTALL_BUILD_DIR="$(mktemp -d)"
 
 LIBTPM_DIR="$(realpath "${SCRIPT_DIR}/../..")"
-LIBTPM_BUILD_DIR="${LIBTPM_DIR}/build"
+LIBTPM_BUILD_DIR="$(mktemp -d)"
 INSTALL_DIR="$(mktemp -d)"
 
+echo INSTALL_DIR=${INSTALL_DIR}
 echo "==> Building main project at ${LIBTPM_DIR}"
 cmake -S "${LIBTPM_DIR}" -B "${LIBTPM_BUILD_DIR}" -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}"
 cmake --build "${LIBTPM_BUILD_DIR}" --parallel
